a57e640af04907bc0b907f94db5e9163a039b19e
BigBadRussian <skripkoae@gmail.com>
2024-01-25 12:09:14 +0400

    * common_functions.py fixed

--- bot_post_single_photo.py
+++ bot_post_single_photo.py
@@ -1,9 +1,14 @@
-from common_functions import arg_parser, create_bot
+import random
+
+from common_functions import arg_parser, create_bot, collect_photo_filenames
 
 
 def post_single_photo():
     bot, chat_id = create_bot()
-    post_photo = arg_parser().filename
+    if arg_parser().filename:
+        post_photo = arg_parser().filename
+    else:
+        post_photo = random.choice(collect_photo_filenames())
     bot.send_photo(chat_id=chat_id, photo=open(f'images/{post_photo}', 'rb'))
 
 
