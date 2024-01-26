03d59b3b1ec72b471de41a8d95fe58beab8d0a01
BigBadRussian <skripkoae@gmail.com>
2024-01-26 16:54:41 +0400

    script has own arg_parser

--- bot_post_single_photo.py
+++ bot_post_single_photo.py
@@ -1,12 +1,19 @@
+import argparse
 import random
+from common_functions import create_bot, collect_photo_filenames
 
-from common_functions import arg_parser, create_bot, collect_photo_filenames
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description='Download spaceX launch photo')
+    parser.add_argument('-n', '--filename', type=str, help='enter photo filename', default=None)
+    args = parser.parse_args()
+    return args
 
 
 def post_single_photo():
     bot, chat_id = create_bot()
-    if arg_parser().filename:
-        post_photo = arg_parser().filename
+    if create_arg_parser().filename:
+        post_photo = create_arg_parser().filename
     else:
         post_photo = random.choice(collect_photo_filenames())
     bot.send_photo(chat_id=chat_id, photo=open(f'images/{post_photo}', 'rb'))
