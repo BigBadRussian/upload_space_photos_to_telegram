ed949a57f9ba17cc54daff4e589451749306bdd4
BigBadRussian <skripkoae@gmail.com>
2024-01-26 17:02:20 +0400

    script has own arg_parser

--- fetch_nasa_apod_photos.py
+++ fetch_nasa_apod_photos.py
@@ -1,7 +1,15 @@
+import argparse
 import os
 import requests
 from dotenv import load_dotenv
-from common_functions import download_image, create_photo_folder, get_image_extension, arg_parser
+from common_functions import download_image, create_photo_folder, get_image_extension
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description='Download spaceX launch photo')
+    parser.add_argument('-c', '--count', type=int, help='enter count of photos', default=5)
+    args = parser.parse_args()
+    return args
 
 
 def get_nasa_apod_image_links(count):
@@ -19,7 +27,7 @@ def get_nasa_apod_image_links(count):
 
 def main():
     create_photo_folder()
-    for i, link in enumerate(get_nasa_apod_image_links(arg_parser().count), 1):
+    for i, link in enumerate(get_nasa_apod_image_links(create_arg_parser().count), 1):
         download_image(link, path_to_file=f"images/nasa_apod_photo_{i}{get_image_extension(link)}")
 
 
