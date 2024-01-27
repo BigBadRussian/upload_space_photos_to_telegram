b8d8c5d0d95c181ccb06842a95f0dcf9b9a5734d
BigBadRussian <skripkoae@gmail.com>
2024-01-27 20:56:23 +0400

    get_nasa_epic_image_links(nasa_token) devided into 2 functions: get_nasa_epic_image_properties returns a list of dicts of different photo properties; def compose_nasa_epic_image_links(nasa_token) compose photo dates and names into downloading url, returns list of these urls

--- fetch_nasa_epic_photos.py
+++ fetch_nasa_epic_photos.py
@@ -4,14 +4,19 @@ from dotenv import load_dotenv
 from common_functions import download_image
 
 
-def get_nasa_epic_image_links(nasa_token):
+def get_nasa_epic_image_properties(nasa_token):
     epic_photos_properties_url = f'https://api.nasa.gov/EPIC/api/natural/images'
     params = {'api_key': nasa_token}
-    epic_photo_archive_url = f'https://api.nasa.gov/EPIC/archive/natural'
     response = requests.get(epic_photos_properties_url, params=params)
     response.raise_for_status()
-    photo_filenames = [f'{item["image"]}.png' for item in response.json()]
-    photo_dates = ['/'.join(item['date'].split()[0].split('-')) for item in response.json()]
+    return response.json()
+
+
+def compose_nasa_epic_image_links(nasa_token):
+    image_properties = get_nasa_epic_image_properties(nasa_token)
+    epic_photo_archive_url = f'https://api.nasa.gov/EPIC/archive/natural'
+    photo_filenames = [f'{item["image"]}.png' for item in image_properties]
+    photo_dates = ['/'.join(item['date'].split()[0].split('-')) for item in image_properties]
     photo_filenames_and_dates = [{'filename': x, 'date': y} for x, y in zip(photo_filenames, photo_dates)]
     epic_photo_urls = [f'{epic_photo_archive_url}/{photo_filenames_and_dates[i]["date"]}/png/'
                        f'{photo_filenames_and_dates[i]["filename"]}'
@@ -24,8 +29,11 @@ def main():
     nasa_token = os.environ['NASA_TOKEN']
     params = {'api_key': nasa_token}
     os.makedirs('images', exist_ok=True)
-    for i, link in enumerate(get_nasa_epic_image_links(nasa_token=nasa_token), 1):
-        download_image(link, params=params, path_to_file=f"images/nasa_epic_photo{i}.png")
+    filename_template = f"images/nasa_epic_photo_"
+    image_extension = ".png"
+    image_links = compose_nasa_epic_image_links(nasa_token=nasa_token)
+    for i, link in enumerate(image_links):
+        download_image(link, params=params, full_filename=f"{filename_template}{i}{image_extension}")
 
 
 if __name__ == "__main__":
