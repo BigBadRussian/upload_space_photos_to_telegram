5c07f4823fb3cbd7affffb44459344b773be34d2
BigBadRussian <skripkoae@gmail.com>
2024-01-28 10:25:12 +0400

    .parse_args > moved to def main

--- bot_post_single_photo.py
+++ bot_post_single_photo.py
@@ -10,22 +10,22 @@ def set_cli_args():
     parser = argparse.ArgumentParser(description='telegram bot sends random space photo into tg channel')
     parser.add_argument('-n', '--filename', type=str, help='enter photo filename',
                         default=random.choice(collect_photo_filenames()))
-    args = parser.parse_args()
-    return args
+    return parser
 
 
-def post_single_photo(bot, chat_id):
-    post_photo = set_cli_args().filename
+def post_single_photo(bot, chat_id, post_photo):
     with open(f'images/{post_photo}', 'rb') as photo:
         bot.send_photo(chat_id=chat_id, photo=photo)
 
 
 def main():
+    parser = set_cli_args()
+    cli_args = parser.parse_args()
     load_dotenv()
     chat_id = os.environ['TG_CHAT_ID']
     bot_token = os.environ['TG_BOT_TOKEN']
     bot = telegram.Bot(bot_token)
-    post_single_photo(bot=bot, chat_id=chat_id)
+    post_single_photo(bot=bot, chat_id=chat_id, post_photo=cli_args.filename)
 
 
 if __name__ == '__main__':
